9699f7d99586c4a8ff5e91f9b98606f489247aa3
julbroersen <julianabroersen2@gmail.com>
2025-03-18 18:51:43 +0100

    added axios, small css fixes, added states to display weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      date: "Tuesday 18:48",
+      city: response.data.city,
+      icon: response.data.condition.icon_url,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search for a city"
+                className="search-bar form-control w-100"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="search-button btn w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search for a city"
-              className="search-bar form-control w-100"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="search-button btn w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Halifax</h1>
-      <ul>
-        <li>Tuesday 17:00</li>
-        <li>Partly sunny</li>
-      </ul>
-      <div className="row">
-        <div className="col-6 mt-3">
-          <p>
-            <ReactAnimatedWeather
-              icon="PARTLY_CLOUDY_DAY"
-              color="lightpink"
-              size={58}
-              animate="true"
-            />{" "}
-            <span className="temperature">6</span>
+          <div className="col-6 mt-3">
+            <img src={weatherData.icon} alt="Weather icon" className="icon" />
+
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
             <span className="units">°C</span>
-          </p>
-        </div>
-        <div className="col-6 mt-3">
-          <ul>
-            <li>Precipitation:</li>
-            <li>Humidity:</li>
-            <li>Wind: </li>
-          </ul>
+          </div>
+          <div className="col-6 mt-3">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)}km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "63fd08b40a55a7od498ecfac7t28e16f";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
