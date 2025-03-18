6d16606f05e4a0b571805bf7052067f26b3ad224
julbroersen <julianabroersen2@gmail.com>
2025-03-18 16:45:34 +0100

    added footer and links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ export default function App() {
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
+      <footer>
+        <p>
+          Coded by{" "}
+          <a
+            href="https://juliana-broersen.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Juliana Broeresn
+          </a>
+        </p>
+        <p>
+          Open-sourced on{" "}
+          <a
+            href="https://github.com/julbroersen/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
